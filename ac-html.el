74ae2e0108376ff66f40ce4a72511aff07d9d5ce
Zhang Kai Yu <yeannylam@gmail.com>
2014-12-01 20:22:20 +0800

    Add License Header

--- ac-html.el
+++ ac-html.el
@@ -4,9 +4,22 @@
 
 ;; Author: Zhang Kai Yu <yeannylam@gmail.com>
 ;; Version: 0.1.0
-;; Keywords: rails, ruby
+;; Keywords: html, auto-complete, rails, ruby
 ;; Package-Requires: ((auto-complete "1.4"))
 ;; URL: https://github.com/cheunghy/ac-html
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
