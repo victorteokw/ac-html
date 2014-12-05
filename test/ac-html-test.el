ac331de2b8bbe42ffe5b64018a5369fd3588d503
Zhang Kai Yu <yeannylam@gmail.com>
2014-12-05 21:30:18 +0800

    Add test.

--- test/ac-html-test.el
+++ test/ac-html-test.el
@@ -1,4 +1,24 @@
 (require 'ac-html)
 
-(ert-deftest test-truth ()
-  (should (equal t t)))
+(defvar fixture-dir (expand-file-name "fixtures" ac-html-test-dir))
+
+(ert-deftest test-ac-html--load-list-from-file ()
+  "Test `ac-html--load-list-from-file' loads the list separated by \\n.
+If file exist."
+  (let* ((file-name (expand-file-name "list.txt" fixture-dir))
+         (list-from-file (ac-html--load-list-from-file file-name)))
+    (should (equal list-from-file '("foo" "bar" "baz")))))
+
+(ert-deftest test-ac-html--current-html-tag ()
+  "Test `ac-html--current-html-tag' correctly scan the tag user is typing on."
+  (with-temp-buffer
+    (insert "<html><head lang=\"\"\n\n lang=\"\"></head></html>")
+    (goto-char 20)
+    (should (equal (ac-html--current-html-tag) "head"))))
+
+(ert-deftest test-ac-html--current-html-attribute ()
+  "Test `ac-html--current-html-attribute' correctly scan the attribute."
+  (with-temp-buffer
+    (insert "<input type=\"\"></input>")
+    (goto-char 14)
+    (should (equal (ac-html--current-html-attribute) "type"))))
