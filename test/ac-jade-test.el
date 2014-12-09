a3146a93118322350592dd512d5dffb57340b6c9
Zhang Kai Yu <yeannylam@gmail.com>
2014-12-09 11:06:23 +0800

    Add basic test for `ac-jade' and `ac-slim'.

---
+++ test/ac-jade-test.el
@@ -0,0 +1,25 @@
+;;; JADE test
+
+(require 'ac-jade)
+
+(defun test-suite-jade-current-thing-scan (body)
+  "This test suite is for ac-jade."
+  (unwind-protect
+      (progn
+        (with-temp-buffer
+          (insert "html\n  head\n    title= This Title\n  body\n    div")
+          (goto-char 30)
+          (funcall body)))))
+
+(ert-deftest test-ac-html--current-jade-tag ()
+  "Test `ac-html--current-haml-tag' correctly scan the tag user is typing on."
+  (test-suite-jade-current-thing-scan
+   (lambda ()
+     (should (equal (ac-html--current-jade-tag) "title")))))
+
+(ert-deftest test-ac-source-jade-attribute-candidates ()
+  "Test `ac-source-haml-attribute-candidates' return correctly."
+  (test-suite-jade-current-thing-scan
+   (lambda ()
+     (should (equal (nth 2 (ac-source-jade-attribute-candidates))
+                    "contenteditable")))))
