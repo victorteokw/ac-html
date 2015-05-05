0d216d27f975e457c961179b505e56d5f7e7cf38
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-05-05 15:51:15 +0300

    bump version

--- ac-html.el
+++ ac-html.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Zhang Kai Yu
 
 ;; Author: Zhang Kai Yu <yeannylam@gmail.com>
-;; Version: 0.3
+;; Version: 0.4
 ;; Keywords: html, auto-complete, rails, ruby
 ;; Package-Requires: ((auto-complete "1.4") (web-completion-data "0.1"))
 ;; URL: https://github.com/cheunghy/ac-html
